d370e9e7873c4c3b031cb9e47d0d7705f501a128
JDHwjdeogus <zenon_hyden@naver.com>
2022-09-26 17:18:35 +0900

    0926_pm

---
+++ java/소녀시대.java
@@ -0,0 +1,27 @@
+package 자바0926;
+
+public class 소녀시대 {
+ public static String 멤버소개(String 멤버이름) {
+	 return "제 이름은 "+ 멤버이름 +"입니다.";
+ }
+ 
+ public static int 나이조회(int 나이) {
+	 return 나이;
+ }
+ 
+ public static void 활동기간() {
+	 // 접근지정자: public static
+	 // 리턴타입: void
+	 // 메소드명: 활동기간
+	 // 파라미터: 없음
+ }
+ 
+ public static boolean 애인유무(boolean 애인) {
+	// 접근지정자: public static
+	// 리턴타입: boolean
+	// 메소드명: 애인유무
+	// 파라미터: boolean 애인
+	 return 애인;
+ }
+ 
+}
