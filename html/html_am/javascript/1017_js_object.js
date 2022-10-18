9ac67ddca3809321415a8c5930051fa0bcbebf51
JDHwjdeogus <zenon_hyden@naver.com>
2022-10-18 12:47:46 +0900

    1018_am_객체저장_함수에_오류가_있음

--- html/html_am/javascript/1017_js_object.js
+++ html/html_am/javascript/1017_js_object.js
@@ -24,25 +24,41 @@ window.onload = function(){
 }
 
 function btAdd(){
+    if(bt_check){ // 새 버튼 생성 후 값이 등록되어있지 않으면 새 버튼 생성 금지
+        alert("값 먼저 등록해주세요");
+        return;
+    }
+
     var bt = document.createElement("button");
     bt.appendChild(document.createTextNode("세팅" + (++bt_cnt)));
     bt.setAttribute("data-value","0"); // 여기서 value는 변수와 같은 역할이므로 무슨 이름을 주든 상관이 없다.
 
     bt.addEventListener("click", function(){
-        if(this.dataset.value == 0) value_save(this);
-        else table_draw(this);
+        if(this.dataset.value == 0) { // input 값 저장 전
+            value_save(this);
+        }
+        else { // input값 저장 이후
+            table_draw(this);
+        }
     });
 
-    var addBtnResult = document.getElementsByClassName("addBtnResult");
-    addBtnResult[0].appendChild(bt);
-    
+    document.getElementsByClassName("addBtnResult")[0].appendChild(bt);
     bt_check = true;
 }
 
 function table_draw(obj){
     var draw = document.querySelector("#draw");
     var data = tbArray[parseInt(obj.dataset.value) - 1];
-    draw.innerHTML = data.row + " " + data.col + " " + data.wdt + " " + data.hgt;
+    
+    var out = "<table>"
+    for(var i = 1; i <= data.col; i++){
+        out += "<tr>";
+        for(var j = 1; j <= data.row; j++){
+            out += "<td width=" + data.wdt + "height=" + data.hgt + "></td>"
+        }
+        out += "</tr>";
+    }
+    out += "</table>"
 }
 
 function value_save(obj){
@@ -66,87 +82,3 @@ function value_save(obj){
         T_put[i].value = "";
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// function tb(col,row, wd, hg){
-//     this.col=col;
-//     this.row=row;
-//     this.width=wd;
-//     this.height=hg;
-// }
-
-// var tb_list = new Array();
-// var bt_cnt=0;
-// var bt_check=false;
-
-// window.onload=function(){
-//     var add_bt = document.querySelector("#add");
-//     add_bt.addEventListener("click",add_button);
-
-//     var T_put =document.getElementsByClassName("T_put");
-//     for(var i in T_put){
-//         T_put[i].addEventListener("keyup",function(){
-//             if( !bt_check ){
-//                 alert("새 세팅버튼을 추가하세요");
-//                 this.value='';
-//             }
-//         });
-//     }
-// };
-
-// function add_button(){
-//     var bt = document.createElement("button");
-//     bt.appendChild(document.createTextNode("세팅"+(++bt_cnt)));
-//     bt.setAttribute("data-value","0");
-
-//     bt.addEventListener("click",function(){
-//         if( this.dataset.value== 0) //input 값 저장 하기전
-//             value_save(this);
-//         else  // input 값 저장 이후
-//             table_draw(this);
-//     });
-//     document.getElementsByClassName("add_wrap")[0].appendChild(bt);
-//     bt_check=true;
-// }
-
-// function table_draw(obj){
-//     var draw = document.querySelector("#draw");
-
-//     var data = tb_list[parseInt(obj.dataset.value)-1];
-
-//     draw.innerHTML = data.col+" "+data.row+" "+data.width+" "+data.height;
-// }
-
-// function value_save(obj){
-//     var input_label=["행","열","너비","높이"];
-//     var T_put = document.getElementsByClassName("T_put");
-//     for(var i in T_put){
-//         if(T_put[i].value==''){
-//             alert(input_label[i] +" 을 입력하세요");
-//             T_put[i].focus();
-//             return;
-//         }
-//     }
-
-//     var temp = new tb(T_put[0].value, T_put[1].value,T_put[2].value,
-//         T_put[3].value);
-//     tb_list.push(temp);
-
-//     obj.dataset.value=bt_cnt;
-
-//     bt_check=false;
-//     for(var i=0; i<T_put.length; i++){
-//         T_put[i].value='';
-//     }
-    
-// }
