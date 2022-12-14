bb21db3a5696de709af90e46e5c857a5f5070a0f
JDHwjdeogus <unohyden18@gmail.com>
2022-12-14 15:21:29 +0900

    1214_pm

--- java/java_pm/member/src/main/java/com/dw/member/controller/MemberController.java
+++ java/java_pm/member/src/main/java/com/dw/member/controller/MemberController.java
@@ -3,7 +3,10 @@ package com.dw.member.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,7 @@ public class MemberController {
 	@Autowired
 	MemberRepo repo;
 	
+	
 	// 전체조회
 	@GetMapping("/member")
 	public List<Member> callAllMembers(){
@@ -24,12 +28,47 @@ public class MemberController {
 		return repo.findAll();
 	}
 	
+	
 	// 추가
 	@PostMapping("/member")
 	public Member callSaveMember(@RequestBody Member member) {
 		// save == insert
 		member = repo.save(member);
 		return member;
+	}
+	
+	
+	// 삭제
+	@DeleteMapping("/member/{id}")
+	public boolean callDeleteMember(@PathVariable long id) {
+		// deleteById == delete
+		// By == WHERE
+		try {
+			repo.deleteById(id);	// return 타입이 void 임.
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+		
+	}
+	
+	
+	// 수정
+	@PatchMapping("/member")
+	public Member updateMember(@RequestBody Member member) {
+		// save == update or insert
+		// 동일한 pk 값이 있으면 update
+		// 동일한 pk 값이 없으면 insert
+		member = repo.save(member);
+		return member;
+	}
+	
+	
+	// 상세조회
+	@GetMapping("/member/{id}")
+	public Member callMemberById(@PathVariable long id) {
+		// fincById == select * from emp where empno == 333(예시)
+		return repo.findById(id).get();
 	}
 	
 }
--- java/java_pm/member/src/main/java/com/dw/member/controller/MemberPageController.java
+++ java/java_pm/member/src/main/java/com/dw/member/controller/MemberPageController.java
@@ -2,11 +2,13 @@ package com.dw.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+// @Controller: 페이지 이동 || @RestController: 데이터 전달
 @Controller
 public class MemberPageController {
 
-	@GetMapping("/members")
+	@GetMapping("/mpage")
 	public String callMemberPage() {
 		return "members";
 	}
