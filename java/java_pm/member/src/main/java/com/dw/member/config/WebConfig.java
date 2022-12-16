171d0aca1d7524a587bffca55842b5ea3e7df0be
JDHwjdeogus <unohyden18@gmail.com>
2022-12-16 16:29:34 +0900

    1216_pm

---
+++ java/java_pm/member/src/main/java/com/dw/member/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.dw.member.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Config: 설정 || Web Config: 웹 설정
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    @Autowired
+    Interceptor interceptor;    // 만들어둔 Interceptor 파일 불러오기
+    
+    // addInterciptors: 인터셉터를 추가하는 함수를 재정의
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        String[] patterns = {"/login", "/error", "/resources/static/*", "/api/v1/login"};    // 인터셉터 제외 url
+        registry.addInterceptor(interceptor).excludePathPatterns(patterns);
+    }
+}
